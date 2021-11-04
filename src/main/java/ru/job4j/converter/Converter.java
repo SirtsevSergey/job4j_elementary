298b21178d34ef2461d9bb92f272ea8a3fa53671
Сергей <artman99@yandex.ru>
2021-11-05 01:31:36 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,22 +3,22 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float rsl = value / 70; /* формула перевода рублей в евро. */
+        float rsl = value / 70;
         return rsl;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60; /* формула перевода рублей в доллары. */
+        float rsl = value / 60;
         return rsl;
     }
 
     public static float dollarToRuble(float value) {
-        float rsl = 60 * value; /* формула перевода доллар в рубли  . */
+        float rsl = 60 * value;
         return rsl;
     }
 
     public static float euroToRuble(float value) {
-        float rsl = 70 * value; /* формула перевода евро в рубли  . */
+        float rsl = 70 * value;
         return rsl;
     }
 
