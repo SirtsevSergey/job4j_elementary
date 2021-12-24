8c077fd3156d23012bf593e2a8fbfd9f664c807d
Сергей <artman99@yandex.ru>
2021-12-25 01:10:25 +0300

    5. Конвертер валюты [#41583 #223929]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -12,26 +12,11 @@ public class Converter {
         return rsl;
     }
 
-    public static float dollarToRuble(float value) {
-        float rsl = 60 * value;
-        return rsl;
-    }
-
-    public static float euroToRuble(float value) {
-        float rsl = 70 * value;
-        return rsl;
-    }
-
-    public static void main(String[] args) {
+       public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dolar.");
-        float rubleeuro = Converter.euroToRuble(140);
-        System.out.println("140 euro are " + rubleeuro + " ruble.");
-        float rubledollar = Converter.dollarToRuble(140);
-        System.out.println("140 dollar are " + rubledollar + " ruble.");
     }
-
-      }
+}
 
--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -1,14 +1,18 @@
 package ru.job4j;
 
 import org.junit.Test;
+import ru.job4j.converter.Converter;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class MainTest {
     @Test
-    public void whenActionThenResult() {
-        int excepted = 1;
-        int result = 1;
-        assertThat(result, is(excepted));
+    public static void main(String[] args) {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
     }
 }
