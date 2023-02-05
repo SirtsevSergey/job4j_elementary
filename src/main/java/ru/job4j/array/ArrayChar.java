0423a4c04ba7b23979278d8399356821b3a392d7
SirtsevSergey <artman99@yandex.ru>
2023-02-05 20:35:54 +0300

    6.5.1. Поиск минимального числа в массиве. [#179803] исправление билда)

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,17 +1,17 @@
-package ru.job4j.array;
-
-public class ArrayChar {
-
-    public static boolean startsWith(char[] word, char[] pref) {
-        for (int i = 0; i < pref.length; i++) {
-            if (word[i] != pref[i]) {
-                return false;
-            }
-        }
-
-        return true;
-=======
-        return false;
->>>>>>> origin/master
-    }
-}
+//package ru.job4j.array;
+//
+//public class ArrayChar {
+//
+//    public static boolean startsWith(char[] word, char[] pref) {
+//        for (int i = 0; i < pref.length; i++) {
+//            if (word[i] != pref[i]) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+////=======
+////        return false;
+////>>>>>>> origin/master
+//    }
+//}
